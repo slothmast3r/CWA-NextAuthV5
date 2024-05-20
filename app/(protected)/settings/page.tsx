51387642fcff43fb00a3ea6b07997fad73e42202
sloth_mast3r <oskar.straszynski@gmail.com>
2024-05-20 17:32:48 +0200

    logowanie i middleware

---
+++ app/(protected)/settings/page.tsx
@@ -0,0 +1,19 @@
+import { auth, signOut } from "@/auth";
+import React from "react";
+
+export default async function SettigsPage() {
+  const session = await auth();
+  return (
+    <div>
+      {JSON.stringify(session)}
+      <form
+        action={async () => {
+          "use server";
+          await signOut();
+        }}
+      >
+        <button type="submit">Sign out</button>
+      </form>
+    </div>
+  );
+}
---
+++ routes.ts
@@ -0,0 +1,25 @@
+/*
+    an array of routes that are used for public access
+    */
+
+export const publicRoutes = ["/"];
+
+/*
+ an array of routes that are used for authentication
+*/
+
+export const authRoutes = ["/auth/login", "/auth/register"];
+
+/**
+ *  the prefix for the api routes
+ * routes that start with this prefix are considered api routes
+ */
+
+export const apiAuthPrefix = "/api/auth";
+
+/**
+ * the default redirect path after login
+ * @type {string}
+ */
+
+export const DEFAULT_LOGIN_REDIRECT = "/settings";
